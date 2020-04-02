3c6f2f9eb54b18ba4432a9cf135c4472bf9bf33e
Anton Ostroumov <anton.ostroumov@gmail.com>
2020-04-03 00:24:07 +0200

    Portable protoc call (Ubuntu support).

--- server/build.sh
+++ server/build.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 
-/opt/atomic/atomic-protobuf/root/usr/bin/protoc ../common/*.proto --proto_path=../common/ --python_out=./bin
+PATH=/opt/atomic/atomic-protobuf/root/usr/bin/:${PATH}
+
+protoc ../common/*.proto --proto_path=../common/ --python_out=./bin
 
 docker build -t beslim.ai .
