51fc35aa6b63c800f4e48d876d7070e87028e956
El-76 <anton.ostroumov@gmail.com>
2020-04-05 14:42:31 +0300

    Redundant space.

--- server/run-fastcgi.sh
+++ server/run-fastcgi.sh
@@ -8,4 +8,4 @@ else
     RUNTIME=""
 fi
 
-docker run $RUNTIME -d --network beslim.ai --ip 172.88.0.3 -v`pwd`/config.py:/opt/beslim.ai/bin/config.py:ro -v `pwd`/debug/:/opt/beslim.ai/var/run/debug/ beslim.ai /opt/beslim.ai/bin/server.fcgi | awk '{ print("fastcgi: "substr($0, 1, 12)) }'
+docker run $RUNTIME -d --network beslim.ai --ip 172.88.0.3 -v`pwd`/config.py:/opt/beslim.ai/bin/config.py:ro -v`pwd`/debug/:/opt/beslim.ai/var/run/debug/ beslim.ai /opt/beslim.ai/bin/server.fcgi | awk '{ print("fastcgi: "substr($0, 1, 12)) }'
